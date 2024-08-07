f40f9864a5fb559fbf784d822836db90c7880469
Omkar Jadhav <objadhav28@gmail.com>
2024-08-07 13:50:18 +0530

    commit

--- src/main/java/com/yumcourt/model/Contact.java
+++ src/main/java/com/yumcourt/model/Contact.java
@@ -1,22 +1,16 @@
 package com.yumcourt.model;
 
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Contact {
     private long id;
     private long phone;
     private Address address;
-
-    public Contact(long id, long phone, Address address) {
-        this.id = id;
-        this.phone = phone;
-        this.address = address;
-    }
-
-    // Getters and Setters
-    public long getId() { return id; }
-    public void setId(long id) { this.id = id; }
-    public long getPhone() { return phone; }
-    public void setPhone(long phone) { this.phone = phone; }
-    public Address getAddress() { return address; }
-    public void setAddress(Address address) { this.address = address; }
 
 }
