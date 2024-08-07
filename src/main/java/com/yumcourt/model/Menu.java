ad456c0131e76e030f2e50afa51df067fc41b83c
Omkar Jadhav <objadhav28@gmail.com>
2024-08-07 13:51:00 +0530

    commit

--- src/main/java/com/yumcourt/model/Menu.java
+++ src/main/java/com/yumcourt/model/Menu.java
@@ -1,5 +1,13 @@
 package com.yumcourt.model;
 
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Menu {
     private long id;
     private String name;
@@ -7,28 +15,5 @@ public class Menu {
     private double price;
     private Restaurant restaurant;
     private boolean availability;
-
-    public Menu(long id, String name, String description, double price, Restaurant restaurant, boolean availability) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.restaurant = restaurant;
-        this.availability = availability;
-    }
-
-    // Getters and Setters
-    public long getId() { return id; }
-    public void setId(long id) { this.id = id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
-    public double getPrice() { return price; }
-    public void setPrice(double price) { this.price = price; }
-    public Restaurant getRestaurant() { return restaurant; }
-    public void setRestaurant(Restaurant restaurant) { this.restaurant = restaurant; }
-    public boolean isAvailable() { return availability; }
-    public void setAvailability(boolean availability) { this.availability = availability; }
 
 }
